d9c2616cca5319488e86229a62e4cb2518529f6c
Rudi Yardley <contact@rudiyardley.com>
2019-03-03 11:52:59 +1100

    Update images and tests

---
+++ src/App.hooks.optimised.js
@@ -0,0 +1,23 @@
+import React, { useCallback, useState } from "react";
+import Box from "./Box";
+
+function App() {
+  const [isYellow, setIsYellow] = useState(true);
+
+  // handleClick requires information from the current state to do its work.
+  // This means that everytime state changes a new handler is created
+  // This is incredibly common that a handler needs to know something about
+  // the state of the component.
+  const handleClick = useCallback(() => {
+    setIsYellow(isYellow => !isYellow);
+  }, []);
+
+  return (
+    <>
+      <h1>App.hooks.optimized.js</h1>
+      <Box isYellow={isYellow} depth={12} onClick={handleClick} />
+    </>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -3,7 +3,12 @@ import ReactDOM from "react-dom";
 
 import AppHooks from "./App.hooks";
 import AppClass from "./App.class";
+import AppHooksOptimized from "./App.hooks.optimised";
 
-const App = process.env.REACT_APP_HOOKS === "1" ? AppHooks : AppClass;
+const App = {
+  "0": AppClass,
+  "1": AppHooks,
+  "2": AppHooksOptimized
+}[process.env.REACT_APP_HOOKS];
 
 ReactDOM.render(<App />, document.getElementById("root"));
